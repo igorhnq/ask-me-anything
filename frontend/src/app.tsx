55d86a07d0f79fe9d60967a8d9b42d2690ef1280
igorhnq <igorhenrique102@gmail.com>
2025-07-29 21:28:16 -0300

    feat: add react query

--- frontend/src/app.tsx
+++ frontend/src/app.tsx
@@ -2,6 +2,8 @@ import { Toaster } from 'sonner'
 import { RouterProvider, createBrowserRouter } from 'react-router-dom'
 import CreateRoom from './pages/create-room'
 import Room from './pages/room'
+import { queryClient } from './lib/react-query'
+import { QueryClientProvider } from '@tanstack/react-query'
 
 const router = createBrowserRouter([
   {
@@ -16,9 +18,9 @@ const router = createBrowserRouter([
 
 export default function App() {
   return (
-    <>
+    <QueryClientProvider client={queryClient}>
       <Toaster invert richColors />
       <RouterProvider router={router} />
-    </>
+    </QueryClientProvider>
   )
 }
